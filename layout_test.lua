b5100e5ba15f8c14ccce18d27f0fb1a55736523f
Lee Gao <lg342@cornell.edu>
2017-01-03 16:39:16 -0600

    remove old files

--- layout_test.lua
+++
@@ -1,4 +0,0 @@
-local layout = require 'see.layout'
-local undump = require 'see.undump'
-
-print(layout.layout({1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,200,21,23,24,2000,25,26,2000, 23}, 20, 1, 60, ',', nil, true))
